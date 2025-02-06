e01c873a737116f7d89b07b5f198d767c1331553
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-02-06 08:55:10 +0000

    管理者ログイン修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.find_or_create_by!(email: "admin@example.com") do |admin|
+  admin.password = "kk33699"
+  admin.password_confirmation = "kk33699"
+end
