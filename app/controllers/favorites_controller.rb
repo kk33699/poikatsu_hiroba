0bc9a7bb1d8483d59c245daf5d6abff83b2de21f
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-02-09 08:00:28 +0000

    いいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,22 +4,23 @@ class FavoritesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: @post.id)
-
+  
     if favorite.save
-      redirect_to @post, notice: '「いいね」をしました。'
+      redirect_to request.referer || posts_path, notice: '「いいね」をしました。'
     else
-      redirect_to @post, alert: '「いいね」の追加に失敗しました。'
+      redirect_to request.referer || posts_path, alert: '「いいね」の追加に失敗しました。'
     end
   end
-
+  
   def destroy
     @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find_by(post_id: @post.id)
-
+  
     if favorite.destroy
-      redirect_to @post, notice: '「いいね」を解除しました。'
+      redirect_to request.referer || posts_path, notice: '「いいね」を解除しました。'
     else
-      redirect_to @post, alert: '「いいね」の解除に失敗しました。'
+      redirect_to request.referer || posts_path, alert: '「いいね」の解除に失敗しました。'
     end
   end
+  
 end
