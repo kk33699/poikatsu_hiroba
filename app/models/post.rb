de4457cca56ff11614efa839bab06cb30d2d2695
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-02-11 11:40:42 +0000

    レビュー機能星表示確認済

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true
   validates :reward_rate, inclusion: { in: REWARD_RATES, allow_blank: true }
-
+  validates :rate, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_nil: true
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
