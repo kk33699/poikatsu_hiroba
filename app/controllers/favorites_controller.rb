d9ab05097219ad2052508b257846d91b467ad72a
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-02-16 13:07:04 +0000

    フラッシュメッセージ・文言等修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,25 +2,16 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: @post.id)
-  
-    if favorite.save
-      redirect_to request.referer || posts_path, notice: '「いいね」をしました。'
-    else
-      redirect_to request.referer || posts_path, alert: '「いいね」の追加に失敗しました。'
-    end
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: post.id)
+    favorite.save
+    redirect_to request.referer || posts_path, allow_other_host: true
   end
-  
+
   def destroy
-    @post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: @post.id)
-  
-    if favorite.destroy
-      redirect_to request.referer || posts_path, notice: '「いいね」を解除しました。'
-    else
-      redirect_to request.referer || posts_path, alert: '「いいね」の解除に失敗しました。'
-    end
+    post = Post.find(params[:post_id])
+    favorite = current_user.favorites.find_by(post_id: post.id)
+    favorite&.destroy
+    redirect_to request.referer || posts_path, allow_other_host: true
   end
-  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true
-  validates :reward_rate, inclusion: { in: REWARD_RATES, allow_blank: true }
-  validates :rate, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }, allow_nil: true
+  validates :reward_rate, presence: true
+  validates :rate, presence: true
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
