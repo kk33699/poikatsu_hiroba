db0f5c72fea08ecbf697cd4bacab22779d831a9d
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-02-23 10:41:44 +0000

    ゲストユーザーいいね機能制限

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,7 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :restrict_guest_user
+  
   def create
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
@@ -14,4 +15,13 @@ class FavoritesController < ApplicationController
     favorite&.destroy
     redirect_to request.referer || posts_path, allow_other_host: true
   end
+
+  private
+
+  def restrict_guest_user
+    if current_user.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは「いいね」機能を利用できません。'
+    end
+  end
+
 end
