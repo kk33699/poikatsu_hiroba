6a9d1b1c3d6348b152e53eb1ce9a3b01178ff92f
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-06-02 07:10:31 +0000

    管理者用ルーティング・コントローラー追加

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,11 +1,23 @@
 class Admin::PostsController < ApplicationController
   layout 'admin' # 管理者用レイアウト
   before_action :authenticate_admin! # 管理者認証
-  before_action :set_post, only: [:show, :destroy] # 投稿データを取得
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def show 
     @post = Post.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to admin_post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @post.destroy
     redirect_to admin_reviews_path, notice: "投稿を削除しました。"
@@ -16,4 +28,9 @@ class Admin::PostsController < ApplicationController
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :reward_rate, :rate, :tag_list)
+  end
+
 end
