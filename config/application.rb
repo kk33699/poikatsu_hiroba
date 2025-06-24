f2b22e9b0b89df45ef16e13a2f03ac6875f01f19
kk33699 <183782474+kk33699@users.noreply.github.com>
2025-06-24 04:49:18 +0000

    メッセージ日本語⇒デフォルト（英語）に変更

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module PoikatsuHiroba
     config.load_defaults 6.1
 
     config.time_zone = 'Tokyo' # タイムゾーンを日本時間に設定
-    config.i18n.default_locale = :ja # ログイン等メッセージを日本語に
+    # config.i18n.default_locale = :ja # ログイン等メッセージを日本語に エラー発生のためデフォルト（英語）に戻したためコメントアウト
 
     # Configuration for the application, engines, and railties goes here.
     #
